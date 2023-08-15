d1cb904cd2b99cf03a3b5a92d6ddad799a8f89b0
Peyman <118217228+Ptechgithub@users.noreply.github.com>
2023-08-15 23:08:59 +0330

    1

---
+++ lib/2nd.py
@@ -0,0 +1,16 @@
+import os
+import time
+import sys
+# Set color
+G = '\033[1;32m' # Green
+
+def delay_print(s):
+    for c in s:
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.003)
+delay_print
+delay_print (""+G+"╔════════════════════════════════════════╗  ""\n")
+delay_print (""+G+"║ Telegram: @P_tech2024                  ║  ""\n")
+delay_print (""+G+"║ Github: https://github.com/Ptechgithub ║  ""\n")
+delay_print (""+G+"╚════════════════════════════════════════╝  ""\n")
